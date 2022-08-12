0b86f0e1116aa9d02fcf342d252bd4c53e70d512
= <roman14@naver.com>
2022-08-12 20:54:08 +0900

    성과표 목록 조회 쿼리 기록 조회 문제 수정

--- src/main/java/com/ez2db/repository/AchievementRepository.java
+++ src/main/java/com/ez2db/repository/AchievementRepository.java
@@ -1,8 +1,8 @@
 package com.ez2db.repository;
 
-import com.ez2db.entity.AchieveVO;
+import com.ez2db.vo.AchieveVO;
 import com.ez2db.entity.KeyType;
-import com.ez2db.entity.OverallVO;
+import com.ez2db.vo.OverallVO;
 import lombok.RequiredArgsConstructor;
 import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Repository;
@@ -24,26 +24,26 @@ public class AchievementRepository
   @SuppressWarnings("SqlDialectInspection")
   public List<AchieveVO> findAchieveListByUserIdWithKeyTypeWithLevel(String userId, KeyType keyType, int level)
   {
-//    String query = "SELECT new com.ez2db.entity.AchieveVO(r.id, mi.name, r.score, mi.level, mi.rank, r.grade, concat(mi.imageFile.filePath, mi.imageFile.fileOriginName))  " +
-//      "FROM Member m JOIN m.recordList r JOIN r.music mi  " +
-//      "WHERE mi.keyType = :keyType  " +
-//      "AND   mi.level   = :level  " +
+//    String query = "SELECT new com.ez2db.vo.AchieveVO(r.id, mi.name, r.score, mi.level, mi.rank, r.grade, concat(mi.imageFile.filePath, mi.imageFile.fileOriginName))   " +
+//      "FROM Member m JOIN m.recordList r JOIN r.music mi   " +
+//      "WHERE mi.keyType = :keyType   " +
+//      "AND   mi.level   = :level   " +
 //      "AND   m.userId   = :userId ";
 
-//    String query = " " +
-//      "select new com.ez2db.entity.AchieveVO( " +
-//      "         (select coalesce(r.id, '')  FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )  " +
-//      ",        mi.id  " +
-//      ",        mi.name  " +
-//      ",        (select r.score             FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )  " +
-//      ",        mi.rank  " +
-//      ",        (select concat(r.grade, '') FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )  " +
-//      ",        (select r.isAllCool         FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )  " +
-//      ",        (select r.isNoMiss          FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )  " +
-//      ",        concat(mi.imageFile.filePath, mi.imageFile.fileOriginName)  " +
-//      ")  " +
-//      "FROM MusicInfo mi  " +
-//      "WHERE mi.keyType = :keyType  " +
+//    String query = "  " +
+//      "select new com.ez2db.vo.AchieveVO(  " +
+//      "         (select coalesce(r.id, '')  FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )   " +
+//      ",        mi.id   " +
+//      ",        mi.name   " +
+//      ",        (select r.score             FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )   " +
+//      ",        mi.rank   " +
+//      ",        (select concat(r.grade, '') FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )   " +
+//      ",        (select r.isAllCool         FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )   " +
+//      ",        (select r.isNoMiss          FROM Record r WHERE r.music.id = mi.id AND r.member.userId = :userId )   " +
+//      ",        concat(mi.imageFile.filePath, mi.imageFile.fileOriginName)   " +
+//      ")   " +
+//      "FROM MusicInfo mi   " +
+//      "WHERE mi.keyType = :keyType   " +
 //      "AND mi.level = :level ";
 
 //    return em.createQuery(query, AchieveVO.class)
@@ -65,19 +65,23 @@ public class AchievementRepository
       ",      (SELECT f.FILE_ORIGIN_NAME FROM FILE f WHERE mi.FILE_ID = f.FILE_ID) " +
       "FROM MUSIC_INFO mi " +
       "  LEFT JOIN ( " +
-      "      SELECT MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID, MAX(ADD_TIME) " +
-      "      FROM RECORD " +
-      "      GROUP BY MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID " +
+      "    SELECT tr2.RECORD_DETAIL_ID, tr2.MUSIC_INFO_ID, tr2.MEMBER_ID, tr2.ADD_TIME " +
+      "    FROM ( " +
+      "      SELECT MUSIC_INFO_ID, MEMBER_ID, MAX(ADD_TIME) as ADD_TIME " +
+      "      FROM RECORD tr2 " +
+      "      GROUP BY MUSIC_INFO_ID, MEMBER_ID " +
       "      HAVING MEMBER_ID = ( " +
-      "          SELECT MEMBER_ID " +
-      "          FROM MEMBER " +
-      "          WHERE USER_ID = :userId " +
+      "        SELECT MEMBER_ID " +
+      "        FROM MEMBER " +
+      "        WHERE USER_ID = :userId " +
       "      ) " +
-      "  ) r ON mi.MUSIC_INFO_ID = r.MUSIC_INFO_ID " +
+      "    ) tr " +
+      "    LEFT JOIN RECORD tr2 ON tr2.MUSIC_INFO_ID = tr.MUSIC_INFO_ID AND tr2.ADD_TIME = tr.ADD_TIME AND tr2.MEMBER_ID = tr.MEMBER_ID " +
+      "  ) r ON mi.MUSIC_INFO_ID = r.MUSIC_INFO_ID   " +
       "  LEFT JOIN RECORD_DETAIL rd ON rd.RECORD_DETAIL_ID = r.RECORD_DETAIL_ID " +
       "WHERE mi.KEY_TYPE = :keyType " +
       "AND   mi.LEVEL = :level " +
-      "ORDER BY mi.RANK DESC, mi.NAME";
+      "ORDER BY mi.RANK DESC, mi.NAME ";
     
     return resultMapper.list(em.createNativeQuery(nativeQuery)
         .setParameter("userId", userId)
@@ -91,28 +95,28 @@ public class AchievementRepository
   {
     Optional<OverallVO> findOverallVO;
 
-    String nativeQuery = "" +
-      "SELECT AVG(rd.PERCENTAGE) rateAvg " +
-      ",      0 " +
-      ",      SUM(CASE WHEN rd.IS_ALL_COOL = TRUE THEN 1 ELSE 0 END) allCoolCnt " +
-      ",      SUM(CASE WHEN rd.IS_NO_MISS  = TRUE THEN 1 ELSE 0 END) noMissCnt " +
-      ",      SUM(CASE WHEN rd.GRADE       = 'SPPP' THEN 1 ELSE 0 END) spppCnt " +
-      ",      SUM(CASE WHEN rd.GRADE       = 'SPP' THEN 1 ELSE 0 END) sppCnt " +
-      ",      SUM(CASE WHEN rd.GRADE       = 'SP' THEN 1 ELSE 0 END) spCnt " +
-      "FROM MUSIC_INFO mi   " +
-      "  JOIN ( " +
-      "      SELECT MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID, MAX(ADD_TIME) " +
-      "      FROM RECORD " +
-      "      GROUP BY MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID " +
-      "      HAVING MEMBER_ID = (   " +
-      "          SELECT MEMBER_ID   " +
-      "          FROM MEMBER   " +
-      "          WHERE USER_ID = :userId " +
-      "      )   " +
-      "  ) r ON mi.MUSIC_INFO_ID = r.MUSIC_INFO_ID   " +
-      "  JOIN RECORD_DETAIL rd ON rd.RECORD_DETAIL_ID = r.RECORD_DETAIL_ID " +
-      "WHERE mi.KEY_TYPE = :keyType " +
-      "AND   mi.LEVEL = :level " +
+    String nativeQuery = " " +
+      "SELECT AVG(rd.PERCENTAGE) rateAvg  " +
+      ",      0  " +
+      ",      SUM(CASE WHEN rd.IS_ALL_COOL = TRUE THEN 1 ELSE 0 END) allCoolCnt  " +
+      ",      SUM(CASE WHEN rd.IS_NO_MISS  = TRUE THEN 1 ELSE 0 END) noMissCnt  " +
+      ",      SUM(CASE WHEN rd.GRADE       = 'SPPP' THEN 1 ELSE 0 END) spppCnt  " +
+      ",      SUM(CASE WHEN rd.GRADE       = 'SPP' THEN 1 ELSE 0 END) sppCnt  " +
+      ",      SUM(CASE WHEN rd.GRADE       = 'SP' THEN 1 ELSE 0 END) spCnt  " +
+      "FROM MUSIC_INFO mi    " +
+      "  JOIN (  " +
+      "      SELECT MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID, MAX(ADD_TIME)  " +
+      "      FROM RECORD  " +
+      "      GROUP BY MUSIC_INFO_ID, MEMBER_ID, RECORD_DETAIL_ID  " +
+      "      HAVING MEMBER_ID = (    " +
+      "          SELECT MEMBER_ID    " +
+      "          FROM MEMBER    " +
+      "          WHERE USER_ID = :userId  " +
+      "      )    " +
+      "  ) r ON mi.MUSIC_INFO_ID = r.MUSIC_INFO_ID    " +
+      "  JOIN RECORD_DETAIL rd ON rd.RECORD_DETAIL_ID = r.RECORD_DETAIL_ID  " +
+      "WHERE mi.KEY_TYPE = :keyType  " +
+      "AND   mi.LEVEL = :level  " +
       "GROUP BY r.MEMBER_ID";
 
     try
