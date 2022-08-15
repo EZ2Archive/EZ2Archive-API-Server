901b9958961d36137d8a4985f110f9c9ff8184da
= <roman14@naver.com>
2022-08-15 14:40:58 +0900

    리퍼지토리 인터페이스 전환

--- src/main/java/com/ez2db/repository/FileRepository.java
+++ src/main/java/com/ez2db/repository/FileRepository.java
@@ -1,43 +1,8 @@
 package com.ez2db.repository;
 
-import com.ez2db.entity.File;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import java.util.Optional;
-
 @Repository
-@RequiredArgsConstructor
-public class FileRepository
+public interface FileRepository
 {
-  private final EntityManager em;
-
-  public Long save(File file)
-  {
-    em.persist(file);
-
-    return file.getId();
-  }
-
-  public Optional<File> findByFileName(String fileName)
-  {
-    Optional<File> file = Optional.empty();
-
-    try
-    {
-      file = Optional.ofNullable(
-        em.createQuery("SELECT f FROM File f WHERE f.fileName = :fileName", File.class)
-          .setParameter("fileName", fileName)
-          .getSingleResult()
-      );
-    }
-    catch( NoResultException e )
-    {
-      // ignore
-    }
-
-    return file;
-  }
 }
