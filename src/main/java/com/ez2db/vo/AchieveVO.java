7ba28d46d71612e57d954b8f699b754008b12669
= <roman14@naver.com>
2022-08-16 11:35:15 +0900

    데이터베이스 변경으로 인한 QLRM 전용 생성자 추가

--- src/main/java/com/ez2db/vo/AchieveVO.java
+++ src/main/java/com/ez2db/vo/AchieveVO.java
@@ -3,6 +3,7 @@ package com.ez2db.vo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 @SuppressWarnings("unused")
@@ -49,10 +50,18 @@ public class AchieveVO
   }
 
   /**
-   * QLRM 전용 Long/BigInteger 변환 생성자
+   * QLRM 전용 생성자 (H2)
    */
   public AchieveVO(BigInteger recordId, BigInteger musicInfoId, String name, int score, Double percentage, int rank, String difficulty, String grade, boolean isAllCool, boolean isNoMiss, String imagePath)
   {
     this(recordId.longValue(), musicInfoId.longValue(), name, score, percentage, rank, difficulty, grade, isAllCool, isNoMiss, imagePath);
+  }
+
+  /**
+   * QLRM 전용 생성자 (MySQL)
+   */
+  public AchieveVO(BigInteger recordId, BigInteger musicInfoId, String name, BigInteger score, Double percentage, int rank, String difficulty, String grade, BigDecimal isAllCool, BigDecimal isNoMiss, String imagePath)
+  {
+    this(recordId.longValue(), musicInfoId.longValue(), name, score.intValue(), percentage, rank, difficulty, grade, isAllCool.intValue() > 0, isNoMiss.intValue() > 0, imagePath);
   }
 }
