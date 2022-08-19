12f771dc105c99c8355286e63536016184034175
= <roman14@naver.com>
2022-08-19 18:01:16 +0900

    파일다운로드 오류로 인한 메세지 컨버터 오버라이드 메소드 삭제

--- src/main/java/com/ez2db/common/config/WebMvcConfig.java
+++ src/main/java/com/ez2db/common/config/WebMvcConfig.java
@@ -2,15 +2,10 @@ package com.ez2db.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer
@@ -31,15 +26,6 @@ public class WebMvcConfig implements WebMvcConfigurer
     registry.addMapping("/**")
       .allowedOrigins("*");
   }
-
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
-  {
-    // Global Encode Setting
-    converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
-    WebMvcConfigurer.super.configureMessageConverters(converters);
-  }
-
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry)
   {
