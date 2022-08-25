f4b04df9803f7d5f3647a7d8fdd1c39c824b1389
= <roman14@naver.com>
2022-08-25 20:05:53 +0900

    의존성 주입 lombok 으로 수정

--- src/main/java/com/ez2archive/common/handler/crypt/PasswordCryptHandler.java
+++ src/main/java/com/ez2archive/common/handler/crypt/PasswordCryptHandler.java
@@ -2,15 +2,12 @@ package com.ez2archive.common.handler.crypt;
 
 import com.ez2archive.common.crypt.PasswordCryptor;
 import com.ez2archive.common.crypt.HexConverter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class PasswordCryptHandler
 {
   private final PasswordCryptor cryptor;
-
-  public PasswordCryptHandler(PasswordCryptor cryptor)
-  {
-    this.cryptor = cryptor;
-  }
 
   public String encode(String password, long salt)
   {
