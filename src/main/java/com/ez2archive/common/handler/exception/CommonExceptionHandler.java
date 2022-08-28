d07eb10a6d0bc94de32ee6916aadd8e76ec407fa
= <roman14@naver.com>
2022-08-28 23:11:10 +0900

    예외 핸들러 케이스 추가

--- src/main/java/com/ez2archive/common/handler/exception/CommonExceptionHandler.java
+++ src/main/java/com/ez2archive/common/handler/exception/CommonExceptionHandler.java
@@ -3,6 +3,8 @@ package com.ez2archive.common.handler.exception;
 import com.ez2archive.common.exception.auth.AuthenticationException;
 import com.ez2archive.common.exception.auth.AuthorizationException;
 import com.ez2archive.common.exception.business.BadRequestException;
+import com.ez2archive.common.exception.business.RequestTimeoutException;
+import com.ez2archive.common.exception.business.ResourceNotFoundException;
 import com.ez2archive.common.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -52,6 +54,34 @@ public class CommonExceptionHandler
   {
     log.debug(e.getMessage(), e);
     return ResponseEntity.badRequest().body(
+      CommonResponse.failed(e)
+    );
+  }
+
+  /**
+   * 요청 타임아웃 핸들러 requestTimeout
+   * @param e
+   * @return
+   */
+  @ExceptionHandler(RequestTimeoutException.class)
+  public ResponseEntity<CommonResponse<?>> handlerBadRequestException(RequestTimeoutException e)
+  {
+    log.debug(e.getMessage(), e);
+    return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body(
+      CommonResponse.failed(e)
+    );
+  }
+
+  /**
+   * 콘텐츠 없음 핸들러 noContent
+   * @param e
+   * @return
+   */
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<CommonResponse<?>> handlerResourceNotFoundException(ResourceNotFoundException e)
+  {
+    log.debug(e.getMessage(), e);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
       CommonResponse.failed(e)
     );
   }
